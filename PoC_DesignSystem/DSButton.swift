5c343fe4344236427f4d34e1b455860ca5ee55e8
Raphael Pedrini Velasqua <Raphzz@Raphael-Buildit-Macbook.local>
2018-12-12 15:32:01 +0000

    Initial commit

---
+++ PoC_DesignSystem/DSButton.swift
@@ -0,0 +1,21 @@
+//
+//  DSButton.swift
+//  PoC_DesignSystem
+//
+//  Created by Raphael Pedrini Velasqua on 12/12/2018.
+//  Copyright © 2018 raph. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DSButton: UIButton {
+
+    required public init?(coder aDecoder: NSCoder) {
+
+        super.init(coder: aDecoder)
+
+        self.backgroundColor = UIColor.blue
+
+    }
+}
---
+++ PoC_DesignSystem/PoC_DesignSystem.h
@@ -0,0 +1,19 @@
+//
+//  PoC_DesignSystem.h
+//  PoC_DesignSystem
+//
+//  Created by Raphael Pedrini Velasqua on 12/12/2018.
+//  Copyright © 2018 raph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PoC_DesignSystem.
+FOUNDATION_EXPORT double PoC_DesignSystemVersionNumber;
+
+//! Project version string for PoC_DesignSystem.
+FOUNDATION_EXPORT const unsigned char PoC_DesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PoC_DesignSystem/PublicHeader.h>
+
+
